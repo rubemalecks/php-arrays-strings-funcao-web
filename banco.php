5bf3b5d6107f6509390d8cd131a5b7fbb82d74de
Rubem Álecks <rubemalecks@gmail.com>
2023-09-17 19:29:51 -0300

    ...

---
+++ banco.php
@@ -0,0 +1,27 @@
+<?php
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
+}
+
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
+exibeMensagem('Olá mundoooo');
